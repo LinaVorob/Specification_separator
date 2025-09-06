a40567cb100c68613748d21a1e6d45622a6b4ac7
lina.luks <linavi96@yandex.ru>
2025-09-06 17:44:28 +0300

    Отсительная спецификация без исправления нумерации.

--- config/config.py
+++ config/config.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 from typing import Tuple
 
-FINAL_FILE_NAME: str = "{}_ОТРЕДАКТИРОВАННЫЙ.xlsx"
+FINAL_FILE_NAME: str = "_ОТРЕДАКТИРОВАННЫЙ.xlsx"
 LOG_FILE_NAME: str = "{}_ЛОГ.txt"
 ABSOLUTE_SHEET_NAME: str = "Абсолютная спецификация"
 RELATIVE_SHEET_NAME: str = "Относительная спецификация"
@@ -9,3 +9,4 @@ PROJECT_PATH: Path = Path(__file__).parent.parent
 NECESSARY_COLUMNS: Tuple[str, ...] = ('ITEM NO.', 'НАИМЕНОВАНИЕ', 'ОБОЗНАЧЕНИЕ', 'ИМЯ_РАБОЧЕГО_ФАЙЛА', 'PART NUMBER',
                                  'РАЗДЕЛ', 'СПОСОБ ИЗГОТОВЛЕНИЯ', 'МАТЕРИАЛ', 'ПРИМЕЧАНИЯ', 'ЗАКАЗ НА СТОРОНЕ')
 WORK_FORMATS: Tuple[str, ...] = ('.xlsx', '.xls')
+NOT_NEED_COLUMNS: Tuple[str, ...] = ('PART NUMBER',)
